9fa129716a8374cacd042e99064870ccb2598631
Caner Candan <caner@candan.fr>
2015-03-27 12:16:28 +0100

    + added model fields, added model fields test

---
+++ tests/model_fields/output/myapp/admin.py
@@ -0,0 +1,74 @@
+# xyz
+# Copyright (C) 2014  xyz developers <admin@localhost.lh> (see AUTHORS)
+#
+# All rights reserved.
+
+from django.contrib.contenttypes import generic
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from django.db.models import Count
+from . import models, forms
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ( 'id', 'name', )
+    search_fields = ( 'name', )
+    ordering = ( 'name', )
+
+    def get_queryset(self, request):
+        qs = super(CategoryAdmin, self).get_queryset(request)
+        return qs
+
+    def save_related(self, request, form, formsets, chance):
+        super(CategoryAdmin, self).save_related(request, form, formsets, change)
+        obj = form.instance
+        obj.save()
+
+admin.site.register(models.Category, CategoryAdmin)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ( 'id', 'name', )
+    search_fields = ( 'name', )
+    ordering = ( 'name', )
+
+    def get_queryset(self, request):
+        qs = super(TagAdmin, self).get_queryset(request)
+        return qs
+
+    def save_related(self, request, form, formsets, chance):
+        super(TagAdmin, self).save_related(request, form, formsets, change)
+        obj = form.instance
+        obj.save()
+
+admin.site.register(models.Tag, TagAdmin)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ( 'id', 'name', 'email', )
+    search_fields = ( 'name', )
+    ordering = ( 'name', )
+
+    def get_queryset(self, request):
+        qs = super(AuthorAdmin, self).get_queryset(request)
+        return qs
+
+    def save_related(self, request, form, formsets, chance):
+        super(AuthorAdmin, self).save_related(request, form, formsets, change)
+        obj = form.instance
+        obj.save()
+
+admin.site.register(models.Author, AuthorAdmin)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ( 'id', 'name', 'slug', 'enabled', 'category', 'author', 'quantity', 'price', 'status', 'description', 'picture', )
+    list_filter = ( 'category', 'author', 'tags', 'status', )
+    filter_horizontal = ( 'tags', )
+    prepopulated_fields = { 'slug': ('name',), }
+    search_fields = ( 'name', 'slug', 'category__name', 'author__name', 'tags__name', )
+    ordering = ( 'name', 'slug', )
+
+    def get_queryset(self, request):
+        qs = super(ProductAdmin, self).get_queryset(request)
+        return qs
+
+    def save_related(self, request, form, formsets, chance):
+        super(ProductAdmin, self).save_related(request, form, formsets, change)
+        obj = form.instance
+        obj.save()
+
+admin.site.register(models.Product, ProductAdmin)
---
+++ tests/model_fields/output/myapp/models.py
@@ -0,0 +1,69 @@
+# xyz
+# Copyright (C) 2014  xyz developers <admin@localhost.lh> (see AUTHORS)
+#
+# All rights reserved.
+
+from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from select_multiple_field.models import SelectMultipleField
+
+class Category(models.Model):
+    name = models.CharField(_('name'), max_length=200, unique=True)
+
+    def save(self, *args, **kwargs):
+        super(Category, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+class Tag(models.Model):
+    name = models.CharField(_('name'), max_length=200, unique=True)
+
+    def save(self, *args, **kwargs):
+        super(Tag, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+class Author(models.Model):
+    name = models.CharField(_('name'), max_length=200, unique=True)
+    email = models.EmailField(_('email'), blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        super(Author, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+class Product(models.Model):
+    name = models.CharField(_('name'), max_length=200)
+    slug = models.SlugField(_('slug'), max_length=200, blank=True, null=True)
+    enabled = models.BooleanField(_('enabled'), default=True)
+    category = models.ForeignKey('Category', verbose_name=_('category'), blank=True, null=True)
+    author = models.ForeignKey('Author', verbose_name=_('author'), blank=True, null=True)
+    tags = models.ManyToManyField('Tag', verbose_name=_('tags'), blank=True, null=True)
+    quantity = models.IntegerField(_('quantity'), default=0)
+    price = models.FloatField(_('price'), default=0.0)
+    STATUS_CHOICES = (
+        ('not_available', _('Not Available')),
+        ('available', _('Available')),
+        ('draft', _('Draft')),
+        ('withdrawn', _('Withdrawn')),
+    )
+    status = models.CharField(_('status'), choices=STATUS_CHOICES, max_length=200)
+    description = models.TextField(_('description'), blank=True, null=True)
+    picture = models.ImageField(_('picture'), blank=True, null=True)
+    date_created = models.DateField(_('date_created'), auto_now_add=True)
+    date_last_modified = models.DateTimeField(_('date_last_modified'), auto_now=True)
+    ip = models.IPAddressField(_('ip'), blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        super(Product, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
