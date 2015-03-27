479024e5dbc0bee986155e3e0c15d4e4cbb1e102
Caner Candan <caner@candan.fr>
2015-03-27 10:02:31 +0100

    + added add_app test

---
+++ tests/add_app/output/myapp/admin.py
@@ -0,0 +1,11 @@
+# xyz
+# Copyright (C) 2014  xyz developers <admin@localhost.lh> (see AUTHORS)
+#
+# All rights reserved.
+
+from django.contrib.contenttypes import generic
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from django.db.models import Count
+from . import models, forms
+
---
+++ tests/add_app/output/myapp/forms.py
@@ -0,0 +1,5 @@
+# xyz
+# Copyright (C) 2014  xyz developers <admin@localhost.lh> (see AUTHORS)
+#
+# All rights reserved.
+
